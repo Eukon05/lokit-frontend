cf1dc8b83ee9e547f18f75dde7df73360d673dcc
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2026-08-13 13:10:15 +0200

    Added styling to selected menu item (dashboard)

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router";
 
+const linkActiveSwitcher = ({ isActive }: { isActive: boolean }) => isActive ? "is-active" : "";
+
 function Menu(){
     return (
         <div className="menu column is-2 box">
             <p className="menu-label">Select dashboard</p>
             <ul className="menu-list">
-                <li><NavLink to="/users">Users and IdP</NavLink></li>
-                <li><NavLink to="/roles">Roles</NavLink></li>
+                <li><NavLink to="/users" className={linkActiveSwitcher}>Users and IdP</NavLink></li>
+                <li><NavLink to="/roles" className={linkActiveSwitcher}>Roles</NavLink></li>
                 <li><a>Rooms</a></li>
                 <li><a>Cards</a></li>
                 <li><a>Card readers (devices)</a></li>
